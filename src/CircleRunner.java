1e4ebd1c93561643ed5b215968ac4d5c4a159823
BT_3W2_24 <henryw48@nycstudents.net>
2022-10-17 12:27:53 -0400

    added code

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        String info = circle.getInfo();
+        System.out.println(info);
+
+        circle.setRadius(9.2);
+        info = circle.getInfo();
+        System.out.println(info);
+
+        Circle circle = new Circle(5.0);
+        System.out.println(circle.getInfo());
+
+        circle.setRadius(9.2);
+        System.out.println(circle.getInfo());
+
+    }
 }
--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class RightTriangle {
+public class RightTriangle {
+    private double base;
+    private double height;
+
+    public RightTriangle(double base, double height) {
+    this.base = base;
+    this.height = height; }
+
+    public double hypotenuse() {
+        return Math.sqrt((Math.pow(base, 2)) + (Math.pow(height,2)));
+
+    RightTriangle triangle1 = new RightTriangle(3, 4);
+    RightTriangle triangle2 = new RightTriangle(6.5, 10.7);
+    double hypotenuse1 = triangle1.hypotenuse();
+    double hypotenuse2 = triangle2.hypotenuse();
+    System.out.println(hypotenuse1);
+    System.out.println(hypotenuse2);
+    }
 }
